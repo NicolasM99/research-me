f089bbd4dece57dd9c83521a9dee6df88c023df2
Nicolas Moreno Posada <moreno.pnicolas@gmail.com>
2021-06-05 21:17:12 -0500

    data retreived from firestore

--- front/src/app/pages/user-pages/Profile.js
+++ front/src/app/pages/user-pages/Profile.js
@@ -4,18 +4,28 @@ import firebase from "firebase/app";
 import { PATHS } from "../../routes/paths";
 import { useHistory, withRouter } from "react-router";
 import { Link } from "react-router-dom";
+import { firestore } from "../../firebase/Firebase";
 
 const Profile = () => {
   const [user, setUser] = useState("initial");
+  const [topics, setTopics] = useState([]);
   const [loading, setLoading] = useState(true);
   const history = useHistory();
   useEffect(() => {
     setUser(firebase.auth().currentUser);
   }, []);
+
   useEffect(() => {
     if (user) {
       setLoading(false);
-    } else if (user !== "initial") {
+      firestore
+        .collection("users")
+        .doc(user.uid)
+        .get()
+        .then((res) => {
+          console.log(res);
+        });
+    } else if (user !== "initial" && !user) {
       history.push(PATHS.LOGIN);
     }
   }, [user]);
@@ -30,22 +40,24 @@ const Profile = () => {
   };
   return (
     <>
-      {loading ? (
+      {loading && user === "initial" ? (
         <Spin />
       ) : (
-        <Row className="container-centered text-center">
-          <Col>
-            <h1>Hola de nuevo {user.displayName}</h1>
-            <p>Estas son las etiquetas que te interesan.</p>
-            <p>PRUEBA</p>
-            <Button type="primary">
-              <Link to={PATHS.RECOMMENDATIONS}>Recomendaciones</Link>
-            </Button>
-            <br />
-            <br />
-            <Button onClick={() => handleSignOut()}>Cerrar sesión</Button>
-          </Col>
-        </Row>
+        user && (
+          <Row className="container-centered text-center">
+            <Col>
+              <h1>Hola de nuevo {user.displayName}</h1>
+              <p>Estas son las etiquetas que te interesan.</p>
+              <p>PRUEBA</p>
+              <Button type="primary">
+                <Link to={PATHS.RECOMMENDATIONS}>Recomendaciones</Link>
+              </Button>
+              <br />
+              <br />
+              <Button onClick={() => handleSignOut()}>Cerrar sesión</Button>
+            </Col>
+          </Row>
+        )
       )}
     </>
   );
